761a1fd7dda973a19ddc384787501cd84cbcd26f
laloncheraa <eduardoperezzavala12@gmail.com>
2026-04-28 17:14:50 -0600

    Add files via upload

--- docs/config.js
+++ docs/config.js
@@ -1,1 +1,13 @@
-
+window.APP_CONFIG = {
+  mode: "demo",
+  companyName: "DataTek",
+  worksheetName: "Plan_2026",
+  excelFileName: "Capacitaciones Tecnicas 2026 MTK.xlsm",
+  sharepointSite: "Viajes 2026 MTK Capacitacion Tecnica",
+  usersListName: "Usuarios Dashboard Capacitaciones Tecnicas",
+  sharepointLibraryUrl: "PEGAR_AQUI_URL_DE_SHAREPOINT",
+  usersListUrl: "PEGAR_AQUI_URL_DE_MICROSOFT_LIST",
+  regions: ["Os", "Norte", "Bajío", "Centro", "Sureste", "Golfo"],
+  rolesWithFullAccess: ["Admin"],
+  mockPassword: "demo123"
+};
